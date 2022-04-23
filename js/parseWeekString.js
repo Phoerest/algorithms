79b8e2b46ce3b56526a0fd2e25655d90d5962de6
Phoerest <68276366+Phoerest@users.noreply.github.com>
2022-04-24 02:31:09 +0300

    initial commit

---
+++ js/parseWeekString.js
@@ -0,0 +1,65 @@
+/*
+	Parse a week string.
+	https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#parse-a-week-string
+*/
+function parseWeekString(input) {
+	//1, 2
+	const asciiDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	let position = 0;
+	//3
+	let yearResult = '';
+	while (asciiDigits.includes(input[position])) yearResult += input[position++];
+	if (yearResult.length < 4) return false;
+	const year = parseInt(yearResult);
+	//4
+	if (year < 1) return false;
+	//5
+	if (
+		position >= input.length - 1 ||
+		input[position] !== '-'
+	) {
+		return false;
+	} else position++;
+	//6
+	if (
+		position >= input.length - 1 ||
+		input[position] !== 'W'
+	) {
+		return false;
+	} else position++;
+	//7
+	let weekResult = '';
+	while (asciiDigits.includes(input[position])) weekResult += input[position++];
+	if (weekResult.length !== 2) return false;
+	const week = parseInt(weekResult);
+	//8
+	const maxWeek = weeksInYear(year);
+	//9
+	if (
+		week < 1 ||
+		week > maxWeek
+	) return false;
+	//10
+	if (position <= input.length - 1) return false;
+	//11
+	return { year, week };
+}
+/*
+	Get weeks in year
+	https://stackoverflow.com/a/18479176
+*/
+function getWeekNumber(d) {
+	d = new Date(+d);
+	d.setHours(0, 0, 0, 0);
+	d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+
+	const yearStart = new Date(d.getFullYear(), 0, 1);
+	const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+	return [d.getFullYear(), weekNo];
+}
+
+function weeksInYear(year) {
+	const d = new Date(year, 11, 31);
+	const week = getWeekNumber(d)[1];
+	return week == 1 ? 52 : week;
+}
