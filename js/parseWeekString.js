287857a623065e56c47d434173b1c861c8c8b865
Phoerest <68276366+Phoerest@users.noreply.github.com>
2022-04-24 22:15:29 +0300

    Check input type

--- js/parseWeekString.js
+++ js/parseWeekString.js
@@ -3,6 +3,7 @@
 	https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#parse-a-week-string
 */
 function parseWeekString(input) {
+	if (typeof input !== 'string') return false;
 	//1, 2
 	const asciiDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 	let position = 0;
